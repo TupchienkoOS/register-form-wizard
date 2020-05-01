5f79393674943cd34c8dd145f509851edf7d5925
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-05-01 21:43:00 +0400

    Updates

---
+++ precache-manifest.ada56ce8f89fe771d681a76828a0cbc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53bf282b51659dd1d32eefa9109e9b9",
+    "url": "/register-form-wizard/index.html"
+  },
+  {
+    "revision": "d6b439b8610870fb5ce3",
+    "url": "/register-form-wizard/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "1046861037b251f72a86",
+    "url": "/register-form-wizard/static/css/main.8b96ad95.chunk.css"
+  },
+  {
+    "revision": "d6b439b8610870fb5ce3",
+    "url": "/register-form-wizard/static/js/2.c1cc87b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/register-form-wizard/static/js/2.c1cc87b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1046861037b251f72a86",
+    "url": "/register-form-wizard/static/js/main.00d0d0a0.chunk.js"
+  },
+  {
+    "revision": "e338abce3e72c1cd10bf",
+    "url": "/register-form-wizard/static/js/runtime-main.09602b39.js"
+  },
+  {
+    "revision": "e32bba19733cc82e8634e5cde2bd292b",
+    "url": "/register-form-wizard/static/media/Blank-Profile-Icon.e32bba19.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/register-form-wizard/precache-manifest.01d73d87494489c810e311a484db03fd.js"
+  "/register-form-wizard/precache-manifest.ada56ce8f89fe771d681a76828a0cbc4.js"
 );
 
 self.addEventListener('message', (event) => {
