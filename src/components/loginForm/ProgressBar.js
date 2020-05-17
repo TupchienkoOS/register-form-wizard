10aa13263501ff568a72fd2fbf1172a21aa98942
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-05-17 23:18:12 +0400

    step instead page

--- src/components/loginForm/ProgressBar.js
+++ src/components/loginForm/ProgressBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ProgressBar = ({ page, confirmed }) => {
+const ProgressBar = ({ step, confirmed }) => {
   return (
     <div className="col-8 w-50">
       <ul className="progressbar">
-        <li className={page > 1 ? "active" : null}>Basic</li>
-        <li className={page > 2 ? "active" : null}>Contacts</li>
-        <li className={page > 3 ? "active" : null}>Avatar</li>
+        <li className={step > 1 ? "active" : null}>Basic</li>
+        <li className={step > 2 ? "active" : null}>Contacts</li>
+        <li className={step > 3 ? "active" : null}>Avatar</li>
         <li className={confirmed ? "active" : null}>Finished</li>
       </ul>
     </div>
